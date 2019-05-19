f1e8ee989a9a069a923175f6531bd003538d3a6e
Mike Hacker <desertsofcacti@gmail.com>
2019-05-19 18:00:47 -0500

    render mealsTable

--- calorie-counter/src/scripts/index.js
+++ calorie-counter/src/scripts/index.js
@@ -4,9 +4,10 @@ const MEALS = [
     { description: 'Breakfast', calories: 460 },
     { description: 'Snack', calories: 180 },
     { description: 'Lunch', calories: 600 },
+    { description: 'Dinner', calories: 1200 },
 ];
 
-const { td, th, tr, tbody } = tags;
+const { td, th, tr, tbody, thead, table } = tags;
 
 function cell(tag, className, value) {
     return tag({className}, value);
@@ -21,11 +22,36 @@ function mealRow(className, meal) {
 
 function mealsBody(className, meals) {
     const rows = R.map(R.partial(mealRow, ['stripe-dark']), meals);
-    return tbody({ className }, rows);
+    const rowsWithTotal = R.append(totalRow(meals), rows);
+    return tbody({ className }, rowsWithTotal);
 }
+
+const headerRow = tr([
+    cell(th, 'pa2 tl', 'Meal'),
+    cell(th, 'pa2 tr', 'Calories'),
+]);
+
+const mealHeader = thead(headerRow);
+
+function totalRow(meals) {
+    const total = R.pipe(
+        R.map(meal => meal.calories), 
+        R.reduce((acc, calories) => acc + calories, 0))(meals);
+    return tr({ className: 'bt b' }, [
+        cell(td, 'pa2 tr', 'Total:'),
+        cell(td, 'pa2 tr', total),
+    ]);
+};
+
+function mealsTable(meals) {
+    return table({ className: 'mw5 center w-100 collapse' }, [
+        mealHeader,
+        mealsBody('', meals),
+    ]);
+};
 
 const node = document.getElementById('app');
 
-const view = mealsBody('', MEALS);
+const view = mealsTable(MEALS);
 
 node.appendChild(view);
